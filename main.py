21977ef282e2759f223a56afc3f2202fbc3b5905
Buskes, Arthur Lee <57105606+albuskes@users.noreply.github.com>
2022-05-02 13:08:02 -0400

    Update to allow shooting update from mbed

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
+"""
 from mbedrpc import *
 
 serdev = '/dev/tty.usbmodem1102'
 s = serial.Serial(serdev)
 mbed = SerialRPC(serdev, 9600)
-f.read()
+"""
+import serial
+
+def toShoot(rawInput):
+    """
+    Determines the value of shoot off of mbedInput
+    Input: 
+    rawInput (bytes[]) : the raw bytes sent from mbed
+    """
+    strInput = str(rawInput)
+    shootChar = strInput[2]
+    if shootChar == "F":
+        shoot = False
+    else:
+        shoot = True
+    return shoot;
+
+
+def main():
+    shoot = False
+    ser = serial.Serial()
+    ser.baudrate = 9600
+    ser.port = '/dev/tty.usbmodem1102'
+    ser.open()
+
+    while(1):
+        rawInput = ser.readline()
+        shoot = toShoot(rawInput)
+        print(shoot)
+        
+if __name__ == "__main__":
+    main()
+
+
 
